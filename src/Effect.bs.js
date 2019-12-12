b26fd41e72215d3d5cb033179f056b9c37b260a6
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-12-12 11:27:37 -0500

    Async increment works

--- src/Effect.bs.js
+++ src/Effect.bs.js
@@ -2,15 +2,30 @@
 
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
-var CamlinternalOO = require("bs-platform/lib/js/camlinternalOO.js");
+var Caml_option = require("bs-platform/lib/js/caml_option.js");
 
 function useReducer(initialState, reducer, interpreter) {
   var match = React.useState((function () {
           return initialState;
         }));
+  var setState = match[1];
+  var state = match[0];
+  var dispatch = function (action) {
+    var match = Curry._2(reducer, state, action);
+    var nextEffect = match[1];
+    var nextState = match[0];
+    Curry._1(setState, (function (param) {
+            return nextState;
+          }));
+    if (nextEffect !== undefined) {
+      return Curry._2(interpreter, Caml_option.valFromOption(nextEffect), dispatch);
+    } else {
+      return /* () */0;
+    }
+  };
   return /* tuple */[
-          match[0],
-          match[1]
+          state,
+          dispatch
         ];
 }
 
@@ -23,63 +38,73 @@ function incrementComplete(param_0) {
 }
 
 function reducer(state, action) {
-  if (action) {
+  if (typeof action === "number") {
+    if (action !== 0) {
+      return /* tuple */[
+              {
+                count: state.count + 1 | 0
+              },
+              undefined
+            ];
+    } else {
+      return /* tuple */[
+              state,
+              /* Increment */[incrementComplete]
+            ];
+    }
+  } else {
     return /* tuple */[
             {
               count: action[0]
             },
             undefined
           ];
-  } else {
-    return /* tuple */[
-            state,
-            /* Increment */[incrementComplete]
-          ];
   }
 }
 
-function interpreter(state, effect) {
+function interpreter(effect, dispatch) {
   var incrementComplete = effect[0];
-  return setTimeout((function (param) {
-                reducer(state, Curry._1(incrementComplete, 5));
-                return /* () */0;
-              }), 2000);
+  setTimeout((function (param) {
+          return Curry._1(dispatch, Curry._1(incrementComplete, 5));
+        }), 1000);
+  return /* () */0;
 }
-
-var class_tables = /* Cons */[
-  0,
-  0,
-  0
-];
 
 function testReducer(param) {
-  if (!class_tables[0]) {
-    var $$class = CamlinternalOO.create_table(0);
-    var env = CamlinternalOO.new_variable($$class, "");
-    var env_init = function (env$1) {
-      var self = CamlinternalOO.create_object_opt(0, $$class);
-      self[env] = env$1;
-      return self;
-    };
-    CamlinternalOO.init_class($$class);
-    class_tables[0] = env_init;
-  }
-  return Curry._1(class_tables[0], 0);
+  var match = reducer({
+        count: 1
+      }, /* IncrementComplete */[2]);
+  var match$1 = match[0].count;
+  var message = match$1 !== 2 ? "Failed" : "Passed";
+  console.log(message);
+  return /* () */0;
 }
 
 function Effect(Props) {
   var match = useReducer({
         count: 0
       }, reducer, interpreter);
-  return React.createElement("div", undefined, String(match[0].count));
+  var dispatch = match[1];
+  return React.createElement("div", undefined, React.createElement("button", {
+                  onClick: (function (param) {
+                      return Curry._1(dispatch, /* SyncIncrement */1);
+                    })
+                }, "Sync increment"), React.createElement("button", {
+                  onClick: (function (param) {
+                      return Curry._1(dispatch, /* RequestIncrement */0);
+                    })
+                }, "Async increment"), String(match[0].count));
 }
 
 var requestIncrement = /* RequestIncrement */0;
+
+var syncIncrement = /* SyncIncrement */1;
 
 var make = Effect;
 
 exports.Reffect = Reffect;
 exports.requestIncrement = requestIncrement;
+exports.syncIncrement = syncIncrement;
 exports.incrementComplete = incrementComplete;
 exports.reducer = reducer;
 exports.interpreter = interpreter;
--- src/Effect.re
+++ src/Effect.re
@@ -1,17 +1,19 @@
 [@bs.val] external setTimeout: (unit => unit, int) => float = "setTimeout";
 
 module Reffect {
-  let useReducer = (initialState, reducer, interpreter) => {
+  let useReducer = (initialState: 's, reducer: ('s, 'a) => ('s, option('e)), interpreter: ('e, ('a) => unit) => unit): ('s, ('a) => unit) => {
     let (state, setState) = React.useState(() => initialState);
 
-    (state, setState)
-    // let dispatch = (action) => {
-    //   let (nextState, nextEffect) = reducer(state, action)
-    //   setState(nextState);
-    //   interpreter(nextEffect, dispatch);
-    // };
+    let rec dispatch = (action: 'a): unit => {
+      let (nextState, nextEffect) = reducer(state, action)
+      setState(_ => nextState);
+      switch(nextEffect) {
+        | Some(effect) => interpreter(effect, dispatch);
+        | None => ()
+      };
+    };
 
-    // (state, dispatch)
+    (state, dispatch)
   };
 };
 
@@ -20,35 +22,55 @@ type state = { count: int };
 [@bs.deriving accessors]
 type action =
   | RequestIncrement
+  | SyncIncrement
   | IncrementComplete(int);
 
 type effect =
-  | Increment((int) => action);
+  | Increment((int) => action);  
 
 let reducer = (state: state, action: action): (state, option(effect)) => {
   switch(action) {
   | RequestIncrement => (state, Some(Increment(incrementComplete)))
-  | IncrementComplete(count) => ({ count: count }, None);
+  | SyncIncrement => ({ count: state.count + 1}, None)
+  | IncrementComplete(count) =>
+    ({ count: count }, None);
   };
 };
 
-let interpreter = (state, effect) => {
+let interpreter = (effect, dispatch: ('a) => unit): unit => {
   switch(effect) {
-  | Increment(incrementComplete) => 
-    setTimeout(() => ignore(reducer(state, incrementComplete(5))), 2_000)
+  | Increment(incrementComplete) =>
+    let _ = setTimeout(() => 
+      incrementComplete(5)->dispatch,
+      1_000
+    );
   };
 };
 
 let testReducer = () => {
+  let state = { count: 1 };
+  let (nextState, _) = reducer(state, IncrementComplete(2));
 
+  let message = 
+    switch(nextState.count) {
+    | 2 => "Passed"
+    | _ => "Failed"
+    };
+
+  Js.log(message);
 };
+
+// let apply = (action) => {
+//   interpreter(reducer(state, action))
+// };
 
 [@react.component]
 let make = () => {
   let (state, dispatch) = Reffect.useReducer({ count: 0 }, reducer, interpreter);
 
   <div>
-    <button onClick={_ => dispatch(RequestIncrement)} />
+    <button onClick={_ => dispatch(SyncIncrement)}>{React.string("Sync increment")}</button>  
+    <button onClick={_ => dispatch(RequestIncrement)}>{React.string("Async increment")}</button>
     {React.string(string_of_int(state.count))}
   </div>
 };
